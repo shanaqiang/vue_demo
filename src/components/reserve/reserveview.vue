0463f4b38fb0e8ea047d984c4287da597a8bbec2
sunqi <1137365177@qq.com>
2019-11-25 09:56:18 +0800

    sq 添加预定路由  2019.11.25

---
+++ src/components/reserve/reserveview.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="container">
+    <div style="width: 400px;" class="border-1">
+      <el-steps :active="active" finish-status="success" simple style="background-color: white;margin-top: 20px;">
+        <el-step title="预定您的行程"></el-step>
+        <el-step title="确认并支付"></el-step>
+      </el-steps>
+    </div>
+    <el-col :span="10"
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "reserveview",
+    data() {
+      return {
+        active: 1
+      };
+    },
+    methods: {
+
+    },
+  }
+</script>
+
+<style scoped>
+
+</style>
